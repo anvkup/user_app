5a40ff8f6d54a1df672018471e69d0a048b729af
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2023-01-20 21:41:54 +0530

    published @github

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Navbar from './components/Navbar'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Navbar></Navbar>
+      </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import { BottomNavigation, Text, View } from 'react-native-paper';
+
+export default function Navbar() {
+const index=1;
+const routes=''
+    return (
+        <BottomNavigation
+        navigationState={{index,routes}}
+        />
+    )
+}
+
