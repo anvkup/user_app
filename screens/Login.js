95fec3f1054d264884ca08f57b0e64052c6e52ad
vishal-1230 <vishalvishwajeet422@gmail.com>
2023-01-22 00:28:51 +0530

    .

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Button, Input } from "@ui-kitten/components"
-import { Image } from "react-native"
+import { Image, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Svg, SvgUri } from "react-native-svg"
 
@@ -11,7 +11,7 @@ export default function Login (){
     return (
         <SafeAreaView style={{display: 'flex', justifyContent: 'center', width: '100%', height: '100%', paddingHorizontal: 30}}>
             <Image source={require('../assets/logo.jpeg')} style={{width: 200, height: 200, top: 0, alignSelf: 'center', marginBottom: 70, marginTop: -70}} />
-            <Input placeholder='Phone Number' label='Enter Your Phone Number' keyboardType='numeric' value={phone} onChangeText={setphone} style={{marginBottom: 20}} />
+            <Input placeholder='Phone Number' label='Enter Your Phone Number' accessoryLeft={()=>{return (<Text style={{color: '#666'}}> +91 </Text>)}} keyboardType='numeric' value={phone} onChangeText={setphone} style={{marginBottom: 20}} />
             <Button>GET OTP</Button>
         </SafeAreaView>
     )
