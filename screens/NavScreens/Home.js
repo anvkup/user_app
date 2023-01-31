e658be26d76fb9d032565538790f8dfbfb4cf7e1
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2023-01-31 11:49:42 +0530

    commitxyza

--- screens/NavScreens/Home.js
+++ screens/NavScreens/Home.js
@@ -23,12 +23,12 @@ export default function Home(props, { navigation }) {
             setitemsList(data)
             let newObj = {}
             // console.log(itemsList);
-            await Promise.all(data.map(async(i)=>{
+            await Promise.all(data.map(async (i) => {
                 const response2 = await fetch(`http://156.67.219.185:8000/api/items/getItemDetails?itemId=${i['itemId']}`)
                 const data2 = await response2.json()
                 // console.log(data2);
 
-                newObj[data2['itemId']]=data2
+                newObj[data2['itemId']] = data2
             }))
             setitemsDetails(newObj)
             setitemsDetailsLoaded(true)
@@ -42,35 +42,42 @@ export default function Home(props, { navigation }) {
     return (
         // <SafeAreaView>
         <ScrollView>
-            <ImageSlider caroselImageContainerStyle={{ marginTop: -50 }} data={[
-                { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/fresh.webp' },
-                { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/sale30.webp' },
-                { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/sale.jpg' }
-            ]} autoPlay={true} />
+            <ImageSlider
+                caroselImageContainerStyle={{ marginTop: -50 }}
+                data={[
+                    { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/fresh.webp' },
+                    { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/sale30.webp' },
+                    { img: 'http://156.67.219.185:8000/api/getFile?uri=assets/offers/sale.jpg' }
+                ]}
+                autoPlay={true}
+                timer={5000}
+                preview={false}
+                // showHeader={false}
+            />
             <Text style={{ marginTop: -30, marginLeft: 16, fontWeight: '800', fontSize: 16 }}>Freshly Arrived Vegies</Text>
-            <View style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', }} id={props.cart}>
+            <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', }} id={props.cart}>
                 {
-                    itemsDetailsLoaded ? itemsList.map((i)=>{
+                    itemsDetailsLoaded ? itemsList.map((i) => {
                         console.log(props.cart);
                         console.log('========================================');
                         return (
-                            <Card style={styles.card} id={a} key={i['itemId']} status={i['quantity']>10 ? 'primary': i['quantity']==0 ? 'danger': 'warning'}>
-                                <Image source={{uri: `http://156.67.219.185:8000/api/getFile?uri=${itemsDetails[i['itemId']]['itemImage']}`}} style={styles.cardImage} />
+                            <Card style={styles.card} id={a} key={i['itemId']} status={i['quantity'] > 10 ? 'primary' : i['quantity'] == 0 ? 'danger' : 'warning'}>
+                                <Image source={{ uri: `http://156.67.219.185:8000/api/getFile?uri=${itemsDetails[i['itemId']]['itemImage']}` }} style={styles.cardImage} />
                                 <Text style={styles.cardText}>{itemsDetails[i['itemId']]['itemName']}</Text>
                                 <Text style={styles.cardPrice}>$ {i['price']} per/kg</Text>
-                                <Text status={i['quantity']>10 ? 'primary': i['quantity']==0 ? 'danger': 'warning'} style={styles.inStock}>{i['quantity']>10 ? 'In Stock': i['quantity']==0 ? 'Out Of Stock': 'Few left in Stock'}</Text>
+                                <Text status={i['quantity'] > 10 ? 'primary' : i['quantity'] == 0 ? 'danger' : 'warning'} style={styles.inStock}>{i['quantity'] > 10 ? 'In Stock' : i['quantity'] == 0 ? 'Out Of Stock' : 'Few left in Stock'}</Text>
                                 {
-                                    props.cart[i['itemId']]==null || props.cart[i['itemId']]==0 ? 
-                                    <Button appearance="outline" disabled={i['quantity']!=0 ? false:true} style={styles.addBtn} onPress={()=>{let obj=props.cart; seta(i['itemId']*100); obj[i['itemId']]=1; console.log("OBJ==", obj); props.setCart(obj); console.log("New Cart=", props.cart);}} accessoryLeft={() => { return (<Entypo name="plus" style={{ fontSize: 20, color: i['quantity']!=0 ? "#00bb00":'#ccc' }} />) }} >Add to Cart</Button> :  
-                                    <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', borderColor: "#00bb00", borderWidth: 2, borderRadius: 5, backgroundColor: "#00bb00", marginTop: 6}}>
-                                        <Entypo name="minus" size={14} style={{alignSelf: 'center', paddingHorizontal: 9, paddingVertical: 8, borderColor: '#00bb00', color: "#fff", backgroundColor: "#00bb00"}} onPress={()=>{let obj=props.cart; seta(Math.floor(Math.random()*1000)+i['itemId']); console.log("OBJ@++", obj); obj[i['itemId']]=props.cart[i['itemId']]-1; console.log(obj); props.setCart(obj)}} />
-                                        <Text style={{fontSize: 14, fontWeight: '700', borderLeftColor: "#00bb00", borderRightColor: '#00bb00', backgroundColor: "#fff", paddingVertical: 6, alignSelf: 'center', borderTopWidth: 0, borderBottomWidth: 0, paddingHorizontal: 14, paddingLeft: 18, borderWidth: 2}}>{props.cart[i['itemId']]}</Text>
-                                        <Entypo name="plus" size={14} style={{alignSelf: 'center', paddingHorizontal: 9, paddingVertical: 8, borderColor: '#00bb00', color: "#fff", backgroundColor: "#00bb00"}} onPress={()=>{let obj=props.cart; seta(Math.floor(Math.random()*1000)+i['itemId']); obj[i['itemId']]=props.cart[i['itemId']]+1; props.setCart(obj)}} />
-                                    </View>
+                                    props.cart[i['itemId']] == null || props.cart[i['itemId']] == 0 ?
+                                        <Button appearance="outline" disabled={i['quantity'] != 0 ? false : true} style={styles.addBtn} onPress={() => { let obj = props.cart; seta(i['itemId'] * 100); obj[i['itemId']] = 1; console.log("OBJ==", obj); props.setCart(obj); console.log("New Cart=", props.cart); }} accessoryLeft={() => { return (<Entypo name="plus" style={{ fontSize: 20, color: i['quantity'] != 0 ? "#00bb00" : '#ccc' }} />) }} >Add to Cart</Button> :
+                                        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', borderColor: "#00bb00", borderWidth: 2, borderRadius: 5, backgroundColor: "#00bb00", marginTop: 6 }}>
+                                            <Entypo name="minus" size={14} style={{ alignSelf: 'center', paddingHorizontal: 9, paddingVertical: 8, borderColor: '#00bb00', color: "#fff", backgroundColor: "#00bb00" }} onPress={() => { let obj = props.cart; seta(Math.floor(Math.random() * 1000) + i['itemId']); console.log("OBJ@++", obj); obj[i['itemId']] = props.cart[i['itemId']] - 1; console.log(obj); props.setCart(obj) }} />
+                                            <Text style={{ fontSize: 14, fontWeight: '700', borderLeftColor: "#00bb00", borderRightColor: '#00bb00', backgroundColor: "#fff", paddingVertical: 6, alignSelf: 'center', borderTopWidth: 0, borderBottomWidth: 0, paddingHorizontal: 14, paddingLeft: 18, borderWidth: 2 }}>{props.cart[i['itemId']]}</Text>
+                                            <Entypo name="plus" size={14} style={{ alignSelf: 'center', paddingHorizontal: 9, paddingVertical: 8, borderColor: '#00bb00', color: "#fff", backgroundColor: "#00bb00" }} onPress={() => { let obj = props.cart; seta(Math.floor(Math.random() * 1000) + i['itemId']); obj[i['itemId']] = props.cart[i['itemId']] + 1; props.setCart(obj) }} />
+                                        </View>
                                 }
-                            </Card> 
+                            </Card>
                         )
-                    }):''
+                    }) : ''
                 }
             </View>
         </ScrollView>
