cccc6f91f0869a23729f00de09f513b76a180c29
vishal-1230 <vishalvishwajeet422@gmail.com>
2023-02-01 09:38:46 +0530

    .

--- screens/FirstTime.js
+++ screens/FirstTime.js
@@ -1,21 +1,48 @@
 import { View } from "react-native"
-import { Input, Text } from "@ui-kitten/components"
+import { useState } from "react"
+import { Button, Input, Text } from "@ui-kitten/components"
 import { SafeAreaView } from "react-native-safe-area-context"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export default (props) => {
+
+    const [firstName, setfirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [email, setemail] = useState('')
+    const [address, setaddress] = useState('')
+    
+    function onSubmit() {
+        AsyncStorage.getItem('token', async (err, result)=>{
+            console.log(result);
+            await fetch('http://192.168.126.137:8000/api/users/createUser', {
+                method: 'post',
+            headers: {
+                token: result,
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                name: firstName+" "+lastName,
+                email: email,
+                address: address
+            })
+            })
+        })
+    }
+
     return (
-        <SafeAreaView>
-            <Text style={{alignSelf: 'center', fontSize: 18, fontWeight: '700', paddingVertical: 15}}>Enter your Details</Text>
-            <Text>First Name</Text>
-            <Input placeholder="First Name" />
-            <Text>Last Name</Text>
-            <Input placeholder="Last Name" />
-            <Text>Phone Number</Text>
-            <Input value="9" />
-            <Text>Email Address</Text>
-            <Input placeholder="Your Email ID (Optional)" />
-            <Text>Default Address</Text>
-            <Input placeholder="Address" />
+        <SafeAreaView style={{height: '100%', flexDirection: 'column', justifyContent: 'space-around'}}>
+            <Text style={{alignSelf: 'center', fontSize: 18, fontWeight: '700', paddingTop: 20}}>Create Your Profile</Text>
+            <Text style={{fontWeight: '700', fontSize: 14, paddingLeft: 25, paddingTop: 20}}>First Name</Text>
+            <Input style={{marginHorizontal: 15}} onChangeText={(text)=>{setfirstName(text)}} placeholder="First Name" />
+            <Text style={{fontWeight: '700', fontSize: 14, paddingLeft: 25, paddingTop: 30}}>Last Name</Text>
+            <Input style={{marginHorizontal: 15}} onChangeText={(text)=>{setlastName(text)}} placeholder="Last Name" />
+            {/* <Text style={{fontWeight: '700', fontSize: 14, paddingLeft: 25, paddingTop: 15}}>Phone Number</Text>
+            <Input style={{marginHorizontal: 15}} value="9" /> */}
+            <Text style={{fontWeight: '700', fontSize: 14, paddingLeft: 25, paddingTop: 30}}>Email Address</Text>
+            <Input style={{marginHorizontal: 15}} onChangeText={(text)=>{setemail(text)}} placeholder="Your Email ID (Optional)" />
+            <Text style={{fontWeight: '700', fontSize: 14, paddingLeft: 25, paddingTop: 30}}>Default Address</Text>
+            <Input style={{marginHorizontal: 15}} onChangeText={(text)=>{setaddress(text)}} placeholder="Address" />
+            <Button style={{marginHorizontal: 50, marginTop: 40, marginBottom: 50}} onPress={onSubmit}>SUBMIT</Button>
         </SafeAreaView>
     )
 }
